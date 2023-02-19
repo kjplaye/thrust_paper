0e8ae5d9065df7bdde750b3bae27e1150ea72299
Kevin Player <kjplaye@gmail.com>
2023-02-19 18:48:58 -0500

    Rindler wedge picture

--- plots.py
+++ plots.py
@@ -1,3 +1,4 @@
+from math import sinh, cosh, exp
 import pylab
 pylab.axes().set_aspect('equal')
 
@@ -5,7 +6,7 @@ sub_u = 'ᵤ'
 sub_v = 'ᵥ' 
 
 
-# Momentum space
+# Massless shell
 pylab.fill([0,1,-1],[0,1,1],color = (0.85,0.85,0.85))
 pylab.plot([-1,1],[-1,1],color = 'black')
 pylab.plot([-1,1],[1,-1],color = 'black')
@@ -16,3 +17,38 @@ pylab.text(-0.25,-0.85,"Shell",fontsize = "xx-large")
 pylab.text(-0.8,0.1, f'p{sub_v} = 0',fontsize = 'xx-large', rotation = -45)
 pylab.text(0.3,0.1, f'p{sub_u} = 0',fontsize = 'xx-large', rotation = 45)
 pylab.text(-0.4,0.8,f'p{sub_u}, p{sub_v} < 0',fontsize = 'xx-large')
+
+
+# RIndler wedge W
+def plot_xi_contour(xi):
+    T = []
+    Z = []
+    for eta_v in range(-500,500):
+        eta = eta_v*0.01
+        t = exp(-xi) * sinh(eta)
+        z = exp(-xi) * cosh(eta)
+        T.append(t)
+        Z.append(z)
+    pylab.plot(Z,T, color = 'blue')
+
+def plot_eta_contour(eta):
+    T = []
+    Z = []
+    for xi_v in range(-100,2000):
+        xi = xi_v*0.01
+        t = exp(-xi) * sinh(eta)
+        z = exp(-xi) * cosh(eta)
+        T.append(t)
+        Z.append(z)
+    pylab.plot(Z,T, color = 'green')
+
+pylab.plot([-1,1],[-1,1],color = 'black')
+pylab.plot([-1,1],[1,-1],color = 'black')
+
+for v in range(30):
+    xi = v * 0.1 + 2
+    plot_xi_contour(xi)
+
+for v in range(30):
+    eta = (v-15) * 0.1
+    plot_eta_contour(eta)
